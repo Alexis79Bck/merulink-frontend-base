3f878f02721211a8066313b7200b82d2901c2f9e
Alexis Mata WebDev <alexedmata@gmail.com>
2023-02-15 15:09:51 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1> Cuerpo Totalmente Vacío </h1>
+    </div>
+  )
+}
+
+
